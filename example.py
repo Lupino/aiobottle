0bbaff255d44cde5a8bbbc903f15f20d0dc6a25a
Ubasic <lmjubuntu@gmail.com>
2013-10-11 14:21:54 +0800

    Add example

---
+++ example.py
@@ -0,0 +1,16 @@
+from asyncbottle import TulipBottle
+import asynchttp
+
+app = TulipBottle()
+
+@app.get('/')
+def example():
+    rsp = yield from asynchttp.request('GET', 'http://python.org')
+    content = yield from rsp.read()
+
+    return content
+
+def main(host='localhost', port='8080'):
+    from bottle import run
+
+    run(app, host = host, port = port, server = 'asyncbottle:TulipServer')
